866dda9d1483368e3998d4f3f2155d818156fa93
Max Wo <maxwo@protonmail.com>
2020-08-23 21:31:27 +1200

    Post

---
+++ src/components/YoutubePlayer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const YoutubePlayer = ({src}) => (
+  <div style={{ position: 'relative', 'width': '100%', height: 0, paddingBottom: '56.25%' }}>
+    <iframe title='Youtube Player' style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }} width='560' height='315'
+            src={src} frameBorder='0'
+            allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowFullScreen/>
+  </div>
+)
+
+export default YoutubePlayer
