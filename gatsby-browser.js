1b64fca1ced0e5a6db96ef5c3aca81d5c110361a
Max Wo <maxwo@protonmail.com>
2020-08-30 00:20:02 +1200

    Refactor

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
+import 'katex/dist/katex.min.css';
+
 import Prism from 'prism-react-renderer/prism';
 
 (typeof global !== 'undefined' ? global : window).Prism = Prism;
-
-require('katex/dist/katex.min.css');
 
 require('prismjs/components/prism-kotlin');
